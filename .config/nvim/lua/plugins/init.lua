6f88173c147cb9fb347105d4cbc8394768843d64
Tanishq Agarwal <tanishqagarwal5414@gmail.com>
2024-05-13 10:33:59 +0530

    added rofi config and made sum changes in nvim too

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -31,6 +31,11 @@ return {
     "neovim/nvim-lspconfig",
   },
   { "stevanmilic/nvim-lspimport" },
+  { "hrsh7th/nvim-cmp"},
+  {"hrsh7th/cmp-nvim-lsp"},
+  {"L3MON4D3/LuaSnip"},
+{"saadparwaiz1/cmp_luasnip"},
+  {"rafamadriz/friendly-snippets"},
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
   -- 	opts = {
