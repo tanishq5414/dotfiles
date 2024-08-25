9adfb1daf81229bb35a7c7cc6ec7802e462f652c
Tanishq Agarwal <tanishqagarwal5414@gmail.com>
2024-08-25 16:30:03 +0530

    added shortcuts to jump tmux

--- .config/scripts/tmux-sessionizer.sh
+++ .config/scripts/tmux-sessionizer.sh
@@ -1,25 +1,28 @@
-# Begin script !/usr/bin/env bash
+#!/usr/bin/env bash
+
+exec > >(tee -a /tmp/tmux-sessionizer-debug.log) 2>&1  # Log all output and errors
 
 if [[ $# -eq 1 ]]; then
-    selected=$1
+    selected="$1"
 else
-    selected=$(find $HOME/dev/mittarv $HOME/dev/personal $HOME $HOME/hdd -mindepth 1 -maxdepth 1 -type d | fzf)
+    selected=$(find "$HOME/dev/mittarv" "$HOME/dev/personal" "$HOME" "$HOME/hdd" -mindepth 1 -maxdepth 1 -type d | fzf)
 fi
 
 if [[ -z $selected ]]; then
     exit 0
 fi
 
-selected_name=$(basename "$selected" | tr . _)
+selected_name=$(basename "$selected" | tr '.' '_')
 tmux_running=$(pgrep tmux)
 
 if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-    tmux -u new-session -s $selected_name -c $selected
+    tmux -u new-session -s "$selected_name" -c "$selected" 2>> /tmp/tmux-sessionizer-debug.log
     exit 0
 fi
 
-if ! tmux has-session -t=$selected_name 2> /dev/null; then
-    tmux -u new-session -ds $selected_name -c $selected
+if ! tmux has-session -t="$selected_name" 2> /dev/null; then
+    tmux -u new-session -ds "$selected_name" -c "$selected" 2>> /tmp/tmux-sessionizer-debug.log
 fi
 
-tmux -u switch-client -t $selected_name
+tmux -u switch-client -t "$selected_name" 2>> /tmp/tmux-sessionizer-debug.log
+
