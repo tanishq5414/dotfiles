84dc2796771ea9f21a461421298cee1155a280db
Tanishq Agarwal <tanishqagarwal5414@gmail.com>
2025-01-30 02:07:11 +0530

    reverting back the comments

--- .config/nvim/lua/plugins/comments.lua
+++ .config/nvim/lua/plugins/comments.lua
@@ -1,61 +1,47 @@
 return {
-  "numToStr/Comment.nvim",
-  lazy = true,  -- Lazy load the plugin
-  dependencies = {
-    {
-      "JoosepAlviste/nvim-ts-context-commentstring",
-      lazy = true,  -- Lazy load this plugin as well
-    },
-  },
-  config = function()
-    local wk = require "which-key"
-
-    -- Add key mappings for comment toggle
-    wk.register({
-      ["<leader>/"] = {
-        "<Plug>(comment_toggle_linewise_current)",
-        desc = "Comment",
-        hidden = true,
-      },
-    })
-
-    wk.register({
-      ["<leader>/"] = {
-        "<Plug>(comment_toggle_linewise_visual)",
-        mode = "v",
-        desc = "Comment",
-        hidden = true,
-      },
-    })
-
-    vim.g.skip_ts_context_commentstring_module = true
-    ---@diagnostic disable: missing-fields
-    require("ts_context_commentstring").setup {
-      enable_autocmd = false,
+    'numToStr/Comment.nvim',
+    opts = {
+        ---Add a space b/w comment and the line
+        padding = true,
+        ---Whether the cursor should stay at its position
+        sticky = true,
+        ---Lines to be ignored while (un)comment
+        ignore = nil,
+        ---LHS of toggle mappings in NORMAL mode
+        toggler = {
+            ---Line-comment toggle keymap
+            line = '<C-_>',
+            ---Block-comment toggle keymap
+            block = 'gbc',
+        },
+        ---LHS of operator-pending mappings in NORMAL and VISUAL mode
+        opleader = {
+            ---Line-comment keymap
+            line = '<C-_>',
+            ---Block-comment keymap
+            block = 'gb',
+        },
+        ---LHS of extra mappings
+        extra = {
+            ---Add comment on the line above
+            above = 'gcO',
+            ---Add comment on the line below
+            below = 'gco',
+            ---Add comment at the end of line
+            eol = 'gcA',
+        },
+        ---Enable keybindings
+        ---NOTE: If given `false` then the plugin won't create any mappings
+        mappings = {
+            ---Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
+            basic = true,
+            ---Extra mapping; `gco`, `gcO`, `gcA`
+            extra = true,
+        },
+        ---Function to call before (un)comment
+        pre_hook = nil,
+        ---Function to call after (un)comment
+        post_hook = nil,
     }
-
-    require("Comment").setup {
-      padding = true,
-      sticky = true,
-      toggler = {
-        line = "<C-_>",
-        block = "gbc",
-      },
-      opleader = {
-        line = "<C-_>",
-        block = "gb",
-      },
-      extra = {
-        above = "gcO",
-        below = "gco",
-        eol = "gcA",
-      },
-      mappings = {
-        basic = true,
-        extra = true,
-      },
-      pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-    }
-  end,
 }
 
